c80ef5d38d294ec005717130d1e42bd6db8aea85
IvanZenatta <ivan@zenatta.com>
2025-10-23 17:08:36 -0600

    Template ready for Next.js widget on Zoho CRM

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://live.zwidgets.com/js-sdk/1.2/ZohoEmbededAppSDK.min.js"></script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
